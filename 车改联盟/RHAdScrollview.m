bd3a99d910db001addd7458d1c9db97e65d471d7
zero <zerozhou10086@gmail.com>
2016-07-04 22:24:09 +0800

    init AdView

---
+++ 车改联盟/RHAdScrollview.m
@@ -0,0 +1,15 @@
+//
+//  RHAdScrollview.m
+//  车改联盟
+//
+//  Created by zero on 16/7/4.
+//  Copyright © 2016年 zero. All rights reserved.
+//
+
+#import "RHAdScrollview.h"
+
+@implementation RHAdScrollview
+
+
+
+@end
